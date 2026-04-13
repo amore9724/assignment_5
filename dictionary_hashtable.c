3c0ca740982f8035dadf2aa1e07135e9b476070a
amore9724 <arya.more.edu4@gmail.com>
2026-04-13 13:04:54 -0700

    Fixed hash table code

--- dictionary_hashtable.c
+++ dictionary_hashtable.c
@@ -32,10 +32,11 @@
 
 struct nlist { /* table entry: */
     struct nlist *next; /* next entry in chain */
-    char *name; /* defined name, can remove */
     int index; // this is the line index in the input text file
     int pid; // the process id. you can use the pid result of wait to lookup in the hashtable
     char *command; // command. This is good to store for when you decide to restart a command
+    int starttime;
+    int finishtime;
 };
 
 #define HASHSIZE 101
